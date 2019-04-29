95c52d92a1316574a27e7bac113ed623b70ae07d
Qt君 <2088201923@qq.com>
2019-04-29 13:39:47 +0800

    加入获取Qt版本号和编译器版本号的功能

---
+++ VersionMode.h
@@ -0,0 +1,38 @@
+﻿#ifndef VERSIONMODE_H
+#define VERSIONMODE_H
+
+#include <QObject>
+
+class VersionMode : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QStringList qtVersionList       READ qtVersionList                            NOTIFY listChanged)
+    Q_PROPERTY(QStringList compilerVersionList READ compilerVersionList                      NOTIFY listChanged)
+    Q_PROPERTY(QString     qtVersion           READ qtVersion       WRITE setQtVersion       NOTIFY statusChanged)
+    Q_PROPERTY(QString     compilerVersion     READ compilerVersion WRITE setCompilerVersion NOTIFY statusChanged)
+
+public:
+    VersionMode();
+
+    QStringList qtVersionList();
+    QStringList compilerVersionList();
+
+
+    QString     qtVersion();
+    void        setQtVersion(const QString &version);
+
+    QString     compilerVersion();
+    void        setCompilerVersion(const QString &version);
+
+signals:
+    void listChanged();
+    void statusChanged();
+
+private:
+    QStringList m_qtVersionList;
+    QStringList m_compilerVersionList;
+    QString m_qtVersion;
+    QString m_compilerVersion;
+};
+
+#endif // VERSIONMODE_H
